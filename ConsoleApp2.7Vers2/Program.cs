85d336a34d891cc11609f779dd2bbcc9315dfe06
Alex Zverev <lordzver007@yandex.ru>
2022-03-28 23:05:42 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2.7Vers2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp2._7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int offset = 2;
+            Console.WriteLine("Введите имя");
+            string name = Console.ReadLine();
+            Console.WriteLine("введите символ");
+            string symbol = Console.ReadLine();
+
+            for (int i = 0; i < name.Length + offset; i++)
+            {
+                Console.Write(symbol);                
+            }
+            Console.WriteLine("\n" + symbol + name + symbol);
+
+            for (int i = 0; i < name.Length + offset; i++)
+            {
+                Console.Write(symbol);
+            }            
+        }
+    }
+}
