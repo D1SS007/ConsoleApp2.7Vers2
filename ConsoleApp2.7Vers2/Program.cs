2b92bfd7c258547dc99c9dda627c9f8bc8a1ab06
D1SS007 <101556566+D1SS007@users.noreply.github.com>
2022-03-28 23:46:51 +0300

    Update Program.cs

--- ConsoleApp2.7Vers2/Program.cs
+++ ConsoleApp2.7Vers2/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace ConsoleApp2._7
 {
@@ -6,23 +6,25 @@ namespace ConsoleApp2._7
     {
         static void Main(string[] args)
         {
-            int offset = 2;
+            int offset = 1;           
+            int yNamePosition = 6;
+
             Console.WriteLine("Введите имя");
             string name = Console.ReadLine();
             Console.WriteLine("введите символ");
             string symbol = Console.ReadLine();
 
-            for (int i = 0; i < name.Length + offset; i++)
+            for (int i = 0; i < 3; i++)
             {
-                Console.Write(symbol);                
-            }
-            
-            Console.WriteLine("\n" + symbol + name + symbol);
-
-            for (int i = 0; i < name.Length + offset; i++)
-            {
-                Console.Write(symbol);
+                Console.Write("\n" + symbol);
+                
+                for (int j = 0; j < name.Length + offset; j++)
+                {
+                    Console.Write(symbol);
+                }                
             }            
+            Console.SetCursorPosition(offset, yNamePosition);
+            Console.WriteLine(name + "\n");
         }
     }
 }
